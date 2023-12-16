c458de154cc956b370f58213efa37ab121683a64
Rochelle Mullen <b00156311@mytudublin.ie>
2023-12-16 13:26:21 +0000

    rm

--- Final/BetaVersion/FinalProject/Assets/Scripts/SpawnManager.cs
+++ Final/BetaVersion/FinalProject/Assets/Scripts/SpawnManager.cs
@@ -19,7 +19,7 @@ public class SpawnManager : MonoBehaviour
     
     // Start is called before the first frame update
     void Start()
-    {if (SceneManager.GetActiveScene().name == "Level1")
+    {if (SceneManager.GetActiveScene().name == "LevelOne")
         {
             //setup for spawning 
             InvokeRepeating("SpawnFoodObjects", delay, boneSpawn);
