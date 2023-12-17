44193e1a53374a305877fdaf3a06288c6c5eb81c
Rochelle Mullen <b00156311@mytudublin.ie>
2023-12-17 11:08:06 +0000

    rm

--- Final/BetaVersion/FinalProject/Assets/Scripts/Spin.cs
+++ Final/BetaVersion/FinalProject/Assets/Scripts/Spin.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
+    // code from challenge 3 in create with code spin speed modified 
     private float spin = 100;
     // Start is called before the first frame update
     void Start()
